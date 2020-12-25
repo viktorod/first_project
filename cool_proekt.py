3c4a170b6767fd1559df94abe217d448b47c0d05
viktorod <66479433+viktorod@users.noreply.github.com>
2020-12-25 14:10:50 +0200

    Update cool_proekt.py

--- cool_proekt.py
+++ cool_proekt.py
@@ -2,7 +2,7 @@
 # Все необходимые библиотеки
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.constants import c 
+
 
 # Объединение ну фактически двух синусоид
 def dopler(v):  
@@ -17,34 +17,34 @@ def dopler(v):
            на вход подается ny - частота в Гц
         """
         # типа просто какие-то формулы из астради
-        lyamda_0 = c/ny 
+        lyamda_0 = 331/ny 
         lyamda = v/ny
-        delta_lyamda = (lyamda - lyamda_0)
-        beta = delta_lyamda/lyamda_0
+        delta_lyamda = lyamda - lyamda_0
+        beta = v/331
         # уравнение синусоиды
-        t = np.linspace(0, 10, 100)
-        y = beta * np.sin(lyamda * t)
+        t = np.linspace(0, 20, 100)
+        y = beta * np.sin(delta_lyamda * t)
         #  украшение синусоиды
         plt.plot(t,y,'-',color = 'b')
     # призыв функии
-    dopler_1(5)
+    dopler_1(100)
     
     def dopler_2(ny):
         
         # тоже самое что и выше
-        lyamda_0 = c/ny 
+        lyamda_0 = 331/ny 
         lyamda = v/ny
-        delta_lyamda = (lyamda - lyamda_0)
-        beta = delta_lyamda/lyamda_0
+        delta_lyamda = lyamda - lyamda_0
+        beta = v/331
         
-        t1 = np.linspace(-5, 0, 100)
+        t1 = -np.linspace(20, 0, 100)
         y1 = (beta * np.sin(lyamda * t1))
         
         plt.plot(t1,y1,'-',color = 'r')
         
-    dopler_2(1)
+    dopler_2(100)
 # призыв объединяющей функции
-dopler(22)
+dopler(0.001)
 
 # Якобы машина которая сигналит
 plt.plot([0],[0], 'o', ms = 10, color='k')
